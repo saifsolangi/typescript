7f9a3b2a0d9609aa7e96b7e67fab14c41d0bd936
Saif Ali <saifsolangi2@gmail.com>
2023-08-28 10:51:29 +0500

    type inference

--- variableMe.js
+++ variableMe.js
@@ -1,2 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var user = "saify";
 console.log(user);
+// number
+// type inference ---> this means the typescript is smart enough to detect the value assigned firstly is the number or string or any other data type 
+var userId = 28089;
+userId;
--- variableMe.ts
+++ variableMe.ts
@@ -2,4 +2,12 @@ let user: string = "saify";
 console.log(user);
 
 
+// number
+
+// type inference ---> this means the typescript is smart enough to detect the value assigned firstly is the number or string or any other data type 
+
+let userId: number = 28089;
+userId
+
+
 export { }
